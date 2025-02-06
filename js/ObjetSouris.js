4dad356764996cf6e76a80cb04f3e59d2bef64b0
lm105079 <matthieu.lentini@etu.unice.fr>
2025-02-06 11:07:30 +0100

    cursor update

--- js/ObjetSouris.js
+++ js/ObjetSouris.js
@@ -1,4 +1,5 @@
 import ObjectGraphique from "./ObjectGraphique.js";
+import { drawCircleImmediat } from "./utils.js";
 
 export default class ObjetSouris extends ObjectGraphique {
     constructor(x, y, w, h, couleur) {
@@ -7,8 +8,19 @@ export default class ObjetSouris extends ObjectGraphique {
 
     draw(ctx) {
         ctx.save();
-        ctx.fillStyle = this.couleur;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.strokeStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(this.x-(this.w + 5), this.y);
+        ctx.lineTo(this.x+(this.w +5), this.y);
+        ctx.moveTo(this.x, this.y-(this.w + 5));
+        ctx.lineTo(this.x, this.y+(this.w + 5));
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.w, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.w / 2, 0, Math.PI * 2);
+        ctx.stroke();
         ctx.restore();
     }
 }   
