cb8cf3320b6a775006de1180048338b8fdeefe81
lm105079 <matthieu.lentini@etu.unice.fr>
2025-02-13 15:17:23 +0100

    adding zqsd keyset

--- js/ecouteurs.js
+++ js/ecouteurs.js
@@ -1,32 +1,32 @@
 function initListeners(inputStates, canvas) {
     window.onkeydown = (event) => {
         //console.log("Touche pressée : " + event.key);
-        if(event.key === "ArrowRight") {
+        if(event.key === "ArrowRight" || event.key === "d") {
             inputStates.ArrowRight = true;
         }
-        if(event.key === "ArrowLeft") {
+        if(event.key === "ArrowLeft" || event.key === "q") {
             inputStates.ArrowLeft = true;
         }
-        if(event.key === "ArrowUp") {
+        if(event.key === "ArrowUp" || event.key === "z") {
             inputStates.ArrowUp = true;
         }
-        if(event.key === "ArrowDown") {
+        if(event.key === "ArrowDown" || event.key === "s") {
             inputStates.ArrowDown = true;
         }
     }
 
     window.onkeyup = (event) => {
         //console.log("Touche relachée : " + event.key);
-        if(event.key === "ArrowRight") {
+        if(event.key === "ArrowRight" || event.key === "d") {
             inputStates.ArrowRight = false;
         }
-        if(event.key === "ArrowLeft") {
+        if(event.key === "ArrowLeft" || event.key === "q") {
             inputStates.ArrowLeft = false;
         }
-        if(event.key === "ArrowUp") {
+        if(event.key === "ArrowUp" || event.key === "z") {
             inputStates.ArrowUp = false;
         }
-        if(event.key === "ArrowDown") {
+        if(event.key === "ArrowDown" || event.key === "s") {
             inputStates.ArrowDown = false;
         }
     }
